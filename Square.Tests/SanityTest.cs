a61486e38c0b93267cd0caf8c708e46cd1496f84
Xingfu Dong <xingfu@squareup.com>
2020-08-26 09:14:01 -0700

    fix TestV2APIException test

--- Square.Tests/SanityTest.cs
+++ Square.Tests/SanityTest.cs
@@ -112,7 +112,7 @@ namespace Square.Tests
 
             Assert.AreEqual(errors[0].Category, "AUTHENTICATION_ERROR");
             Assert.AreEqual(errors[0].Code, "UNAUTHORIZED");
-            Assert.AreEqual(errors[0].Detail, "This request could not be authorized."); 
+            Assert.AreEqual(errors[0].Detail, "The `Authorization` http header of your request was malformed. The header value is expected to be of the format \"Bearer TOKEN\" (without quotation marks), where TOKEN is to be replaced with your access token (e.g. \"Bearer ABC123def456GHI789jkl0\"). For more information, see https://docs.connect.squareup.com/api/connect/v2/#requestandresponseheaders. If you are seeing this error message while using one of our officially supported SDKs, please report this to developers@squareup.com.");
         }
 
         [Test]
