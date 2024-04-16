2a7d2504cb210dce1c7629b25bfda749f7b3094c
autobot <devs-apex+github-pusher@squareup.com>
2024-04-16 22:35:31 +0000

    Generated PR for Release: 36.0.0

--- Square/Models/CatalogCustomAttributeDefinition.cs
+++ Square/Models/CatalogCustomAttributeDefinition.cs
@@ -135,7 +135,7 @@ namespace Square.Models
 
         /// <summary>
         /// The set of `CatalogObject` types that this custom atttribute may be applied to.
-        /// Currently, only `ITEM`, `ITEM_VARIATION`, and `MODIFIER` are allowed. At least one type must be included.
+        /// Currently, only `ITEM`, `ITEM_VARIATION`, `MODIFIER`, `MODIFIER_LIST`, and `CATEGORY` are allowed. At least one type must be included.
         /// See [CatalogObjectType](#type-catalogobjecttype) for possible values
         /// </summary>
         [JsonProperty("allowed_object_types")]
